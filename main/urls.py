fe86f8a1a7044b7b97268863021bd1f03e6c18e4
adhis <tiva.adhisti@ui.ac.id>
2023-11-26 21:14:20 +0700

    crud menu

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, pickup_schedule, crud_menu
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('pickup-schedule', pickup_schedule, name='pickup_schedule'),
+    path('crud-menu', crud_menu, name='crud_menu'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
-def show_main(request):
+def admin_dashboard(request):
     context = {
         'title': 'ADMIN DASHBOARD',
     }
 
-    return render(request, 'main.html', context)
+    return render(request, 'adminDashboard.html', context)
+
+def pickup_schedule(request):
+    context = {
+        'title': 'PICKUP SCHEDULE',
+    }
+    return render(request, 'pickupSchedule.html', context)
+
+def crud_menu(request):
+    context = {
+        'title': 'MENU',
+    }
+    return render(request, 'crudMenu.html', context)
