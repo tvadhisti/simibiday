fe0ca9e5a41e5c3b8858a22b1b3d4a3a4f7a6bbb
adhis <tiva.adhisti@ui.ac.id>
2023-11-26 22:42:04 +0700

    completed feature 5

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from main.views import show_main, pickup_schedule, crud_menu
+from main.views import crud_menu, room, sclass, child_in_class, daily_report, daily_report_form, admin_dashboard, room_form, menu_form, update_menu
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('pickup-schedule', pickup_schedule, name='pickup_schedule'),
     path('crud-menu', crud_menu, name='crud_menu'),
+    path('room', room, name='room'),
+    path('class', sclass, name='class'),
+    path('child-in-class', child_in_class, name='child_in_class'),
+    path('daily-report', daily_report, name='daily_report'),
+    path('daily-report-form', daily_report_form, name='daily_report_form'),
+    path('admin-dashboard', admin_dashboard, name='admin_dashboard'),
+    path('room-form', room_form, name='room_form'),
+    path('menu-form', menu_form, name='menu_form'),
+    path('update-menu', update_menu, name='update_menu'),
 ]
