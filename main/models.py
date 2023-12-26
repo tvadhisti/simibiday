f5e3041d05cd033636945bc0c50776a2eb28bbfd
raisaafadilla <raisa.fadilla@ui.ac.id>
2023-12-26 18:28:24 +0700

    add database

--- main/models.py
+++ main/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class MENU(models.Model):
+    ID = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    Name = models.CharField(max_length=50)
+    Type = models.CharField(max_length=50)
+    class Meta:
+        db_table="menu"
+
+class ROOM(models.Model):
+    RoomNo = models.IntegerField(primary_key=True)
+    Area = models.CharField(max_length=50, null=False)
+    class Meta:
+        db_table="room"
+
+class PROGRAM(models.Model):
+    ProgramID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    Name = models.CharField(max_length=20, null=False)
+    AgeMin = models.IntegerField(null=False)
+    AgeMax = models.IntegerField(null=False)
+    class Meta:
+        db_table="program"
+
+class EXTRACURRICULAR(models.Model):
+    ExtracurricularID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Name = models.CharField(max_length=30, null=False)
+    Day = models.CharField(max_length=15, null=False)
+    Hour = models.IntegerField(null=False)
+    class Meta:
+        db_table="extra_curricular"
+
+class USERS(models.Model):
+    UserID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Password = models.CharField(max_length=20, null=False)
+    PhoneNumber = models.CharField(max_length=15, null=False)
+    FirstName = models.CharField(max_length=20, null=False)
+    LastName = models.CharField(max_length=20, null=False)
+    Gender = models.CharField(max_length=10, null=False)
+    Birthdate = models.DateField(null=False)
+    Address = models.TextField(null=False)
+    class Meta:
+        db_table="users"
+
+class CHILD(models.Model):
+    UserID = models.OneToOneField(USERS, on_delete=models.CASCADE, primary_key=True)
+    DadName = models.CharField(max_length=50)
+    MomName = models.CharField(max_length=50)
+    DadJob = models.CharField(max_length=20)
+    MomJob = models.CharField(max_length=20)
+    class Meta:
+        db_table="child"
+
+class STAFF(models.Model):
+    UserID = models.OneToOneField(USERS, on_delete=models.CASCADE, primary_key=True)
+    NIK = models.CharField(max_length=50)
+    NPWP = models.CharField(max_length=50)
+    BankAccount = models.CharField(max_length=50)
+    BankName = models.CharField(max_length=50)
+    class Meta:
+        db_table="staff"
+
+class CAREGIVER(models.Model):
+    UserID = models.OneToOneField(STAFF, on_delete=models.CASCADE, primary_key=True)
+    class Meta:
+        db_table="caregiver"
+
+class CAREGIVER_CERTIFICATE(models.Model):
+    UserID = models.ForeignKey(USERS, on_delete=models.CASCADE)
+    CertificateNumber = models.CharField(max_length=20, null=True)
+    CertificateName = models.CharField(max_length=20, null=True)
+    CertificateYear = models.CharField(max_length=4, null=True)
+    CertificateOrganizer = models.CharField(max_length=20, null=True)
+    class Meta:
+        unique_together = ('UserID', 'CertificateNumber', 'CertificateName', 'CertificateYear', 'CertificateOrganizer')
+        db_table="caregiver_certificate"
+
--- main/views.py
+++ main/views.py
@@ -1,17 +1,145 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import MENU, ROOM, PROGRAM, EXTRACURRICULAR, CHILD
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
+
+def child_list(request):
+    context = {
+        'title' : 'Child List',
+    }
+    return render(request, 'childList.html', context)
+
+def admin_offered_program(request):
+    programs = PROGRAM.objects.all().order_by('Name')
+    context = {
+        'title' : 'Admin Offered Program',
+        'programs': programs
+    }
+    return render(request, 'adminOfferedProgram.html', context)
+
+@require_POST
+def delete_program(request, id):
+    data = get_object_or_404(PROGRAM, ProgramID=id)
+    data.delete()
+    return JsonResponse({'message': 'Data deleted successfully'})
+
+def crud_extracurricular(request):
+    extraCurriculars = EXTRACURRICULAR.objects.all().order_by('Name')
+    context = {
+        'title' : 'Extracurricular',
+        'extraCurriculars': extraCurriculars
+    }
+    return render(request, 'crudExtracurricular.html', context)
+
+def extracurricular_form(request):
+    if request.method == "POST":
+        Name = request.POST['Name']
+        Day = request.POST['Day']
+        Hour = request.POST['Hour']
+        data = EXTRACURRICULAR(Name=Name, Day=Day, Hour=Hour)
+        data.save()
+        return redirect('crud_extracurricular')
+    else:
+        context = {
+            'title' : 'Extracurricular Form',
+        }
+        return render(request, 'extracurricularForm.html', context)
+
+def extracurricular_detail(request, pk):
+    extraCurricular = EXTRACURRICULAR.objects.get(ExtracurricularID=pk)
+    context = {
+        'title' : 'Extracurricular Detail',
+        'extraCurricular': extraCurricular
+    }
+    return render(request, 'extracurricularDetail.html', context)
+
+def update_extracurricular(request, pk):
+    extraCurricular = EXTRACURRICULAR.objects.get(ExtracurricularID=pk)
+    if request.method == "POST":
+        extraCurricular.Name = request.POST['Name']
+        extraCurricular.Day = request.POST['Day']
+        extraCurricular.Hour = request.POST['Hour']
+        extraCurricular.save()
+        return redirect('crud_extracurricular')
+    else:
+        context = {
+            'title' : 'Update Extracurricular',
+            'extraCurricular': extraCurricular
+        }
+        return render(request, 'updateExtracurricular.html', context)
+@require_POST
+def delete_extracurricular(request, id):
+    data = get_object_or_404(EXTRACURRICULAR, ExtracurricularID=id)
+    data.delete()
+    return JsonResponse({'message': 'Data deleted successfully'})
 
 def crud_menu(request):
+    menus = MENU.objects.all().order_by('Name')
     context = {
         'title': 'MENU',
+        'menus': menus
     }
     return render(request, 'crudMenu.html', context)
 
+def menu_form(request):
+    if request.method == "POST":
+        Name = request.POST['Name']
+        Type = request.POST['Type']
+        data = MENU(Name=Name, Type=Type)
+        data.save()
+        return redirect('crud_menu')
+    else:
+        context = {
+            'title': 'MENU FORM',
+        }
+        return render(request, 'menuForm.html', context)
+
+def update_menu(request,pk):
+    menu = MENU.objects.get(ID=pk)
+    if request.method == "POST":
+        menu.Name = request.POST['Name']
+        menu.Type = request.POST['Type']
+        menu.save()
+        return redirect('crud_menu')
+    else:
+        context = {
+            'title': 'UPDATE {} MENU'.format(menu.Name),
+            'menu': menu
+        }
+        return render(request, 'updateMenu.html', context)
+@require_POST
+def delete_menu(request, id):
+    data = get_object_or_404(MENU, ID=id)
+    data.delete()
+    return JsonResponse({'message': 'Data deleted successfully'})
+
 def room(request):
+    rooms = ROOM.objects.all().order_by('RoomNo')
     context = {
         'title': 'ROOM',
+        'rooms': rooms
     }
     return render(request, 'room.html', context)
+
+def room_form(request):
+    if request.method == "POST":
+        RoomNo = request.POST['RoomNo']
+        Area = request.POST['Area']
+        data = ROOM(RoomNo=RoomNo, Area=Area)
+        data.save()
+        return redirect('room')
+    else:
+        context = {
+            'title': 'ROOM FORM',
+        }
+        return render(request, 'roomForm.html', context)
+    
+@require_POST
+def delete_room(request, room_id):
+    room = get_object_or_404(ROOM, RoomNo=room_id)
+    room.delete()
+    return JsonResponse({'message': 'Room deleted successfully'})
 
 def sclass(request):
     context = {
@@ -27,7 +155,7 @@ def child_in_class(request):
 
 def daily_report(request):
     context = {
-        'title': '[CHILD NAME]’s DAILY REPORT',
+        'title': '[CHILD Name]’s DAILY REPORT',
     }
     return render(request, 'dailyReport.html', context)
 
@@ -43,23 +171,6 @@ def admin_dashboard(request):
     }
     return render(request, 'adminDashboard.html', context)
 
-def room_form(request):
-    context = {
-        'title': 'ROOM FORM',
-    }
-    return render(request, 'roomForm.html', context)
-
-def menu_form(request):
-    context = {
-        'title': 'MENU FORM',
-    }
-    return render(request, 'menuForm.html', context)
-
-def update_menu(request):
-    context = {
-        'title': 'UPDATE [MENU NAME] MENU',
-    }
-    return render(request, 'updateMenu.html', context)
 def login(request):
     context = {
         'title': 'SIMIBIDAY - GROUP CODE',
@@ -139,18 +250,6 @@ def edit_activity(request):
     }
     return render(request, 'editActivity.html', context)
 
-def child_list(request):
-    context = {
-        'title' : 'Child List',
-    }
-    return render(request, 'childList.html', context)
-
-def admin_offered_program(request):
-    context = {
-        'title' : 'Admin Offered Program',
-    }
-    return render(request, 'adminOfferedProgram.html', context)
-
 def payment_history_admin(request):
     context = {
         'title' : 'Payment History',
@@ -168,27 +267,3 @@ def payment_form(request):
         'title' : 'Payment Form',
     }
     return render(request, 'paymentForm.html', context)
-
-def crud_extracurricular(request):
-    context = {
-        'title' : 'Extracurricular',
-    }
-    return render(request, 'crudExtracurricular.html', context)
-
-def extracurricular_detail(request):
-    context = {
-        'title' : 'Extracurricular Detail',
-    }
-    return render(request, 'extracurricularDetail.html', context)
-
-def extracurricular_form(request):
-    context = {
-        'title' : 'Extracurricular Form',
-    }
-    return render(request, 'extracurricularForm.html', context)
-
-def update_extracurricular(request):
-    context = {
-        'title' : 'Update Extracurricular',
-    }
-    return render(request, 'updateExtracurricular.html', context)
